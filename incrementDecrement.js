b6c8da75a61a2f282c133dcfb87c09750348400a
opuopu <nurmdopu428@gmail.com>
2025-10-09 11:47:36 +0600

    doing arithmatic operation

---
+++ incrementDecrement.js
@@ -0,0 +1,8 @@
+let x = 3;
+x++;
+x++;
+let y = 5;
+y--;
+y--;
+console.log(x);
+console.log(y);
---
+++ index.js
@@ -0,0 +1,17 @@
+let value1 = 10;
+let value2 = 20;
+let value3 = 30;
+let value4 = 40;
+let value5 = 50;
+let value6 = 60;
+// addition
+// value1 + value2
+// console based
+// console.log(value1 * value2 * value3 * value4 * value5);
+
+// addition
+let addition = value1 + value2 + value3 + value4 + value5;
+// substraction
+let substraction = value5 - value4 - value3 - value2 - value1;
+console.log('addition is', addition);
+console.log('substraction is', substraction);
