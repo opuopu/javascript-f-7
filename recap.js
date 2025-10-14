c7fef48772735565517576f50afcc0a8b9142bf3
opuopu <nurmdopu428@gmail.com>
2025-10-14 11:27:59 +0600

    recap variable & object

---
+++ recap.js
@@ -0,0 +1,35 @@
+//  variable
+
+// const a = 5;
+// const b = 10;
+// b = 'hello';
+// let sum = a + b;
+// console.log(sum);
+
+// Object recap
+// let sutdent1 = {
+//   // key: value
+//   bangla: 80,
+//   english: 90,
+//   math: 100,
+// };
+// let sutdent0 = {
+//   // key: value
+//   bangla: 80,
+//   english: 90,
+//   math: 100,
+// };
+// let student2 = {
+//   // key: value
+//   bangla: 80,
+//   english: 90,
+//   math: 100,
+// };
+// let sutdent3 = {
+//   // key: value
+//   bangla: 80,
+//   type: 'bangla',
+//   isActive: true,
+//   english: 90,
+//   math: 100,
+// };
