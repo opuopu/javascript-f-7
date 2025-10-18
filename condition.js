9ac7563df2b396505f59d0ae14f9a7ee88cde492
opuopu <nurmdopu428@gmail.com>
2025-10-18 12:24:43 +0600

    working with if else statement

---
+++ condition.js
@@ -0,0 +1,42 @@
+const number = 7;
+
+//  2==2
+//  apple == apple  -> true
+
+//  frutis value is not 2
+//  is 5 is greater than number
+
+//  is 5 is less than number
+// == sign
+//  not equal !=
+// greater than  >
+// less than  <
+// greater than or equal  >=
+// less than or equal  <=
+
+// if (number <= 7) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// const fruits = 'apple';
+// const fruits2 = 'banana';
+
+// //  apple  == banana
+// if (fruits != fruits2) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+const time = 20;
+
+if (time > 20) {
+  console.log('Good evening');
+} else if (time < 15) {
+  console.log('Good afternoon');
+} else if (time <= 10) {
+  console.log('Good day');
+} else {
+  console.log('Good morning');
+}
