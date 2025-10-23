09ba5c747de40d9b8b44d36e772c147f335022df
opuopu <nurmdopu428@gmail.com>
2025-10-23 11:23:22 +0600

    while and do while loop

---
+++ whileloop.js
@@ -0,0 +1,29 @@
+const students = [
+  { name: 'Alice', age: 20, salary: 5000 },
+  { name: 'Bob', age: 22, salary: 6000 },
+  { name: 'Charlie', age: 19, salary: 7000 },
+  { name: 'David', age: 21, salary: 8000 },
+  { name: 'Eve', age: 23, salary: 9000 },
+];
+// let totalAge = 0;
+// let totalSalary = 0;
+// for (let i = 0; i < students.length; i++) {
+//   totalAge = totalAge + students[i].age;
+//   totalSalary = totalSalary + students[i].salary;
+// }
+// let totalSalary = 0;
+// let n = 0;
+// while (n < students.length) {
+//   totalSalary += students[n].salary;
+//   n++;
+// }
+// console.log(totalSalary);
+
+//  --------------- do while loop in javascript
+
+let i = 5;
+
+do {
+  console.log(i);
+  i++;
+} while (i < 4);
